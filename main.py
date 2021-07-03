92503c4b4da7b36088a92c7711e96a4963d25e5d
Dola Ram <dola.nith@gmail.com>
2021-07-03 12:48:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 ##---------------------------------------------------------------------------
 # Importinting the packages
 ##---------------------------------------------------------------------------
-import tensorflow as tf
 import cv2
 import sys
 import time 
@@ -9,25 +8,35 @@ import random
 import numpy as np
 from collections import deque
 from matplotlib import pyplot as plt
+
+##---------------------------------------------------------------------------
+# Import the DEEP_Q CAPSULE NETWORK
+##---------------------------------------------------------------------------
+import tensorflow as tf
+from capsulenetwork import *
+
+##---------------------------------------------------------------------------
+# Import PyGame
+##---------------------------------------------------------------------------
 import pygame
 from ple import PLE
 
-from capsulenetwork import *
-from catchertraining import TrainNetwork4Catcher
-from flappybirdtraining import TrainNetwork4FlappyBird
-from pongtraining import TrainNetwork4Pong
-
 ##---------------------------------------------------------------------------
-# Training of DQCN for the Catcher Pygame 
+# Importting the trading module for each game 
+#//one game of pygame is imported at a time to avoid screen splitting and frame overlapping 
 ##---------------------------------------------------------------------------
+# Select the Game for which you want to train Deep-Q Capsule Network
 GAME  = "CATCHER"
 #GAME = 'FLAPPYBIRD'
 #GAME = 'PONG'
 if(GAME == "CATCHER"):
+    from catchertraining import TrainNetwork4Catcher
     ACTIONS = 3 # number of valid actions for Catcher Game
 elif(GAME == "FLAPPYBIRD"):
+    from flappybirdtraining import TrainNetwork4FlappyBird
     ACTIONS = 2 # number of valid actions Flappy Bird
 elif(GAME == "PONG"):
+    from pongtraining import TrainNetwork4Pong
     ACTIONS = 6 # number of valid actions for Pong Game
 else: 
     print("Select the appropriate Pygame for the capsule Network Training")
